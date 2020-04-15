11c307e9db452bb11d3ad683cc2bd5749658480a
anderson amaya <anderama100@gmail.com>
2020-04-15 16:17:25 -0600

    Updates

---
+++ precache-manifest.079303a7c105778d9c1d55eb0b36f906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238559a932aa8512ade63989d9480cf6",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "2fdd9c68bf319af49445",
+    "url": "/project-4-assessment/static/css/main.c8ae633c.chunk.css"
+  },
+  {
+    "revision": "ae4edd645855b0e8173d",
+    "url": "/project-4-assessment/static/js/2.424eb2da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.424eb2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdd9c68bf319af49445",
+    "url": "/project-4-assessment/static/js/main.47eab2a1.chunk.js"
+  },
+  {
+    "revision": "a399ef99de46212c0a17",
+    "url": "/project-4-assessment/static/js/runtime-main.a3b17b99.js"
+  }
+]);
